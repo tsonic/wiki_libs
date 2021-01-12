59aa6ff8e6712fc521fc420000ff07e6ecc35a34
Wei Tang <14265162+tsonic@users.noreply.github.com>
2021-01-11 20:36:47 -0800

    use nearest neighbor lib

--- knn.py
+++ knn.py
@@ -1,11 +1,11 @@
 import torch
 from wiki_libs.preprocessing import normalize, path_decoration
-from sklearn.neighbors import KDTree
+from sklearn.neighbors import KDTree, NearestNeighbors
 import pandas as pd
 import json
 import numpy as np
 
-def build_knn(emb_file, df_cp, w2v_mimic, use_user_emb = True):
+def build_knn(emb_file, df_cp, w2v_mimic, use_user_emb = True, algorithm = 'brute', k = 10):
     emb_file = path_decoration(emb_file, w2v_mimic = w2v_mimic)
     saved_embeddings = torch.load(emb_file, map_location = 'cpu')
     USER_ID = 'page_id'
@@ -25,11 +25,15 @@ def build_knn(emb_file, df_cp, w2v_mimic, use_user_emb = True):
             left_index = True, right_on = 'page_id')
         .set_index('page_title')
     )
-    kdt = KDTree(np.vstack(df_embedding[f"{'user' if use_user_emb else 'item'}_embedding_normalized"]), leaf_size=100, metric='euclidean')
-    return df_embedding, kdt
+    
+    #kdt = KDTree(np.vstack(df_embedding[f"{'user' if use_user_emb else 'item'}_embedding_normalized"]), leaf_size=100, metric='euclidean')
+
+    nn = NearestNeighbors(n_neighbors=k, algorithm=algorithm, leaf_size=1000, n_jobs=-1, p=2)
+    nn = nn.fit(np.vstack(df_embedding[f"{'user' if use_user_emb else 'item'}_embedding_normalized"]))
+    return df_embedding, nn
 
 
-def top_knn(kdt, df_embedding, k = 10, pos_keys = None, neg_keys = None, use_user_emb = True):
+def top_knn(nn, df_embedding, pos_keys = None, neg_keys = None, use_user_emb = True):
     if pos_keys is None:
         pos_keys = []
     if neg_keys is None:
@@ -42,5 +46,5 @@ def top_knn(kdt, df_embedding, k = 10, pos_keys = None, neg_keys = None, use_use
     for key in neg_keys:
         new_v -= df_embedding.loc[key,emb_name] 
     new_v = normalize(new_v)
-    dist, ind = kdt.query([new_v], k=k, return_distance=True)
+    dist, ind = nn.kneighbors([new_v], return_distance=True)
     return df_embedding.iloc[ind[0],:].assign(dist = dist[0])[['dist']].transpose()
